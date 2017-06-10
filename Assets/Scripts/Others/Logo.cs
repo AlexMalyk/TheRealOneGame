ec346d95ddffabd686119f2162c9e6e793849b3e
AlexMalyk <alex.s.malyk@gmail.com>
2017-06-10 20:13:35 +0300

    Change logo script

--- Assets/Scripts/Others/Logo.cs
+++ Assets/Scripts/Others/Logo.cs
@@ -13,10 +13,15 @@ public class Logo : MonoBehaviour {
     float timeLimit = 2f;
     bool onFinish;
 
+    bool logoAnimationFinished;
+
 	// Use this for initialization
 	void Start () {
         time = 0;
         onFinish = false;
+        logoAnimationFinished = false;
+       
+        StartCoroutine(LoadingCoroutine());
     }
 	
 	// Update is called once per frame
@@ -26,7 +31,8 @@ public class Logo : MonoBehaviour {
             time += Time.deltaTime;
         }
         else if(!onFinish) {
-            StartCoroutine(LogoCoroutine());
+            
+            StartCoroutine(LogoAnimationCoroutine());           
         }
 	}
 
@@ -34,22 +40,40 @@ public class Logo : MonoBehaviour {
         time = 0;
     }
 
-    IEnumerator LogoCoroutine() {
+    IEnumerator LogoAnimationCoroutine() {
+        Debug.Log("LogoAnimation Coroutine starts");
         onFinish = true;
         buttons.SetActive(false);
         GetComponent<Animator>().SetTrigger("Center");
         GetComponent<Animator>().SetTrigger("ShowText");
+        Debug.Log("LogoAnimation Coroutine eye on center");
         yield return CoroutineUtil.WaitForRealSeconds(2f);
         GetComponent<Animator>().SetTrigger("Hide");
-        yield return CoroutineUtil.WaitForRealSeconds(2f);
+        Debug.Log("LogoAnimation Coroutine hide all");
+        yield return CoroutineUtil.WaitForRealSeconds(.5f);
+        logoAnimationFinished = true;
+        Debug.Log("LogoAnimation Coroutine finishes");
 
+        //LocalizationManager.manager.SetLanguage();
+        //while (!LocalizationManager.manager.GetIsReady())
+        //{
+        //    yield return null;
+        //}
+
+        //SceneManager.LoadScene("Game");
+
+    }
+    IEnumerator LoadingCoroutine() {
+        Debug.Log("Loading Coroutine starts");
         LocalizationManager.manager.SetLanguage();
-        while (!LocalizationManager.manager.GetIsReady())
+        Debug.Log("Loading Coroutine set language");
+        Debug.Log("Loading Coroutine before manager=" + LocalizationManager.manager.GetIsReady()+", animation="+ logoAnimationFinished);
+        while (!LocalizationManager.manager.GetIsReady() || !logoAnimationFinished)
         {
-            yield return null;
+            yield return new WaitForSeconds(.05f);
         }
-
+        Debug.Log("Loading Coroutine after  manager=" + LocalizationManager.manager.GetIsReady() + ", animation=" + logoAnimationFinished);
+        Debug.Log("Loading Coroutine load scene");
         SceneManager.LoadScene("Game");
-
     }
 }
